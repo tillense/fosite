b6bf1b7587bc387aba5529ac894000442e629fdc
Jannes Klee <jklee@astrophysik.uni-kiel.de>
2017-12-12 09:51:41 +0100

    added: Doxygen documentation
    - target like before: "make doc"
    - only the framework in order to build the webpages and doxygen
            - produces lots of warnings because of unresolved links
    - the .dox files are still related to the old fosite version and
      need to be rewritten.

---
+++ doc/adjust_names.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "test", $1
+
+awk '{gsub("<div class=\"textblock\">", "")}1' ${1}/doc/html/citelist.html > ${1}/doc/html/citelist_tmp.html
+mv ${1}/doc/html/citelist_tmp.html ${1}/doc/html/citelist.html
+sed -i -- 's/href="index.html"/href="main.html"/g' ${1}/doc/html/*.html
+mv ${1}/doc/html/index.html ${1}/doc/html/main.html
+sed -i -- 's/href="fosite.html"/href="index.html"/g' ${1}/doc/html/*.html
+mv ${1}/doc/html/fosite.html ${1}/doc/html/index.html
+sed -i -- 's/class="code"//g' ${1}/doc/html/*.html
