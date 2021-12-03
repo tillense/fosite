5f7b2138b301ac0fe3259ecbe1b16db729361b1f
Tobias Illenseer <tillense@astrophysik.uni-kiel.de>
2021-12-03 16:39:49 +0100

    new more generic implementation of centrifugal forces

--- sources/sources_rotframe.f90
+++ sources/sources_rotframe.f90
@@ -53,7 +53,8 @@ MODULE sources_rotframe_mod
     TYPE(marray_base), ALLOCATABLE :: cent, &     !< rot. frame centrifugal
                                       centproj, & !< rot.frame centr.3d->2d
                                       cos1, &
-                                      sin1
+                                      sin1, &
+                                      Omez        !< Omega*ez
     INTEGER           :: issphere
   CONTAINS
     PROCEDURE :: InitSources_rotframe
@@ -104,17 +105,68 @@ CONTAINS
       CALL this%Error("sources_rotframe::InitSources", &
          "rotating frame with FARGO seems broken, don't use it together")
 
-!    IF (Physics%VDIM.NE.2) &
-!      CALL this%Error("sources_rotframe::InitSources", &
-!         "Only 2D simulations working with rotating frame at the moment." )
-
-    ALLOCATE(this%accel,this%cent,this%centproj,this%cos1,this%sin1)
-    this%accel = marray_base(Physics%VDIM)
-    this%accel%data1d(:) = 0.
-    this%cent = marray_base(3)
-    this%cent%data1d(:) = 0.
     CALL GetAttr(config, "gparam", this%gparam, 1.0)
     CALL GetAttr(config, "issphere", this%issphere, 0)
+
+    ALLOCATE(this%accel,this%cent,this%centproj,this%cos1,this%sin1,this%Omez)
+    this%accel = marray_base(Physics%VDIM)
+    this%accel%data1d(:) = 0.
+
+    ! compute centrifugal acceleration for rotation around vertical direction
+    ! with center of rotation in r_0, i.e. -Omega**2 * ez x (ez x (r-r_0))
+    this%cent = marray_base(3)  ! 3D centrifugal acceleration
+    this%Omez = marray_base(3)  ! = Omega*ez
+    ! set cartesian components of shifted center of rotation, i.e. r_0x, r_0y, r_0z,
+    ! using this%cent as temporary storage
+    this%cent%data2d(:,1) = Mesh%rotcent(1)
+    this%cent%data2d(:,2) = Mesh%rotcent(2)
+    this%cent%data2d(:,3) = Mesh%rotcent(3)
+    ! compute curvilinear components of shift vector
+    CALL Mesh%Geometry%Convert2Curvilinear(Mesh%bcenter,this%cent%data4d,this%cent%data4d)
+    ! subtract the result from the position vector:
+    ! this gives you the curvilinear components of all vectors pointing
+    ! from the center of rotation to the bary center of any cell on the mesh,
+    ! i.e. r-r_0
+    this%cent%data4d(:,:,:,:) = Mesh%posvec%bcenter(:,:,:,:) - this%cent%data4d(:,:,:,:)
+    ! set cartesian components of Omega*ez within computational domain
+    this%Omez%data2d(:,1:2) = 0.0  ! no x and y component
+    WHERE (Mesh%without_ghost_zones%mask1d(:))
+      this%Omez%data2d(:,3) = Mesh%Omega
+    ELSEWHERE
+      this%Omez%data2d(:,3) = 0.0
+    END WHERE
+    ! compute curvinlinear components of Omega*ez with respect to the given geometry of the mesh
+    CALL Mesh%Geometry%Convert2Curvilinear(Mesh%bcenter,this%Omez%data4d,this%Omez%data4d)
+    ! compute curvilinear components of the centrifual acceleration
+    this%cent = this%Omez.x.this%cent ! = Omega*ez x (r-r0)
+    this%cent = this%cent.x.this%Omez ! = -Omega**2 * ez x (ez x (r-r0) = (Omega*ez x (r-r0)) x Omega*ez
+
+    ! set the projected components of the centrifugal acceleration
+    this%centproj = marray_base(Physics%VDIM) ! dimensionality of velocity vector
+    SELECT CASE(Mesh%VECTOR_COMPONENTS)
+    CASE(VECTOR_X) ! 1D momentum in x-direction (1st curvilinear direction)
+      ! vy = vz = my = mz = 0
+      this%centproj%data2d(:,1) = this%cent%data2d(:,1)
+    CASE(VECTOR_Y) ! 1D momentum in y-direction (2nd curvilinear direction)
+      ! vx = vz = mx = mz = 0
+      this%centproj%data2d(:,1) = this%cent%data2d(:,2)
+    CASE(VECTOR_Z) ! 1D momentum in z-direction (3rd curvilinear direction)
+      ! vx = vy = mx = my = 0
+      this%centproj%data2d(:,1) = this%cent%data2d(:,3)
+    CASE(IOR(VECTOR_X,VECTOR_Y)) ! 2D momentum in x-y-plane
+      ! vz = mz = 0
+      this%centproj%data2d(:,1:2) = this%cent%data2d(:,1:2)
+    CASE(IOR(VECTOR_X,VECTOR_Z)) ! 2D momentum in x-z-plane
+      ! vy = my = 0
+      this%centproj%data2d(:,1) = this%cent%data2d(:,1)
+      this%centproj%data2d(:,2) = this%cent%data2d(:,3)
+    CASE(IOR(VECTOR_Y,VECTOR_Z)) ! 2D momentum in y-z-plane
+      ! vx = mx = 0
+      this%centproj%data2d(:,1:2) = this%cent%data2d(:,2:3)
+    CASE DEFAULT
+      ! full 3D case: centproj = cent
+      this%centproj = this%cent
+    END SELECT
 
     ! define position vectors
     ! for planetery atmosphere no shift of axis possible
@@ -122,42 +174,25 @@ CONTAINS
     SELECT TYPE(geo => Mesh%geometry)
     TYPE IS(geometry_spherical_planet)
       IF (this%issphere.EQ.1) THEN
-        this%centproj = marray_base(3)
-        this%centproj%data1d(:) = 0.
+!         this%centproj = marray_base(3)
+!         this%centproj%data1d(:) = 0.
         this%cos1 = marray_base(3)
         this%cos1%data1d(:) = 0.
  
-        this%centproj%data4d(:,:,:,1) = this%gparam*SIN(Mesh%bcenter(:,:,:,1))*&
-                                COS(Mesh%bcenter(:,:,:,1))
+!         this%centproj%data4d(:,:,:,1) = this%gparam*SIN(Mesh%bcenter(:,:,:,1))*&
+!                                 COS(Mesh%bcenter(:,:,:,1))
         ! for better performance
         this%cos1%data4d(:,:,:,1)  = COS(Mesh%bcenter(:,:,:,1))
         this%cos1%data4d(:,:,:,2)  = COS(Mesh%bcenter(:,:,:,2))
-      END IF
-    TYPE IS(geometry_cylindrical)
-      IF (ABS(Mesh%rotcent(1)).LE.TINY(Mesh%rotcent(1)) &
-       .AND.ABS(Mesh%rotcent(2)).LE.TINY(Mesh%rotcent(2))) THEN
-        ! no shift of point mass: set position vector to Mesh defaults
-        this%cent%data4d(:,:,:,:) = Mesh%posvec%bcenter(:,:,:,:)
-      ELSE
-        ! shifted center of rotation:
-        ! compute curvilinear components of shift vector
-        this%cent%data4d(:,:,:,1) = Mesh%rotcent(1)
-        this%cent%data4d(:,:,:,2) = Mesh%rotcent(2)
-        this%cent%data4d(:,:,:,3) = Mesh%rotcent(3)
-        CALL Mesh%Geometry%Convert2Curvilinear(Mesh%bcenter,this%cent%data4d,this%cent%data4d)
-        ! subtract the result from the position vector:
-        ! this gives you the curvilinear components of all vectors pointing
-        ! from the center of rotation to the bary center of any cell on the mesh
-        this%cent%data4d(:,:,:,:) = Mesh%posvec%bcenter(:,:,:,:) - this%cent%data4d(:,:,:,:)
       END IF
     TYPE IS(geometry_spherical)
         this%cos1 = marray_base()
         this%cos1%data3d(:,:,:) = COS(Mesh%bcenter(:,:,:,2))
         this%sin1 = marray_base()
         this%sin1%data3d(:,:,:) = SIN(Mesh%bcenter(:,:,:,2))
-        this%centproj = marray_base()
-        this%centproj%data4d(:,:,:,1) = Mesh%radius%bcenter(:,:,:)*&
-                                this%Sin1%data3d(:,:,:)
+!         this%centproj = marray_base()
+!         this%centproj%data4d(:,:,:,1) = Mesh%radius%bcenter(:,:,:)*&
+!                                 this%Sin1%data3d(:,:,:)
       CLASS DEFAULT
         CALL this%Error('Sources:Sources_rotframe','Geometry not supported at the moment')    
     END SELECT
@@ -292,7 +327,7 @@ CONTAINS
     !------------------------------------------------------------------------!
     TYPE(sources_rotframe), INTENT(INOUT) :: this
     !------------------------------------------------------------------------!
-    DEALLOCATE(this%cent,this%centproj,this%cos1,this%sin1)
+    DEALLOCATE(this%cent,this%centproj,this%cos1,this%sin1,this%Omez)
     ! deallocation of this%accel is done in inherited destructor
     ! which is called automatically
   END SUBROUTINE Finalize
